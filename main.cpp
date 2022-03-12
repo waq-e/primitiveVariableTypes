3c32dfc4c42c05a208fb7f4c37277f8d5079fe17
waq-e <whomomlikes@hotmail.com>
2022-03-12 00:09:08 -0600

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+
+int main() {
+	// Declaring and initializing an int variable.
+	int a = 9;
+
+	// Declaring a string variable without initializing right away.
+	std::string b;
+
+	// Initializing the string b.
+	b = "Here is a string";
+
+	cout << a << "\n";
+	cout << b << "\n";
+}
